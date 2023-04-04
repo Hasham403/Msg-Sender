a1d01633a3ff210b718d26372bd6531172bd7303
muhammadhasham1805@yahoo.com <muhammadhasham1805@yahoo.com>
2023-04-04 17:15:39 +0500

    Add project files.

---
+++ msg_send/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const string accountSid = "ACa9b1fe99998105748d1819c8aec7530f";
+        const string authToken = "a9b02a06a4f932a1fd3f5e84a8ab2a9e";
+        TwilioClient.Init(accountSid, authToken);
+
+        var message = MessageResource.Create(
+            body: "Hello, how are you",
+            from: new Twilio.Types.PhoneNumber("+15854969817"),
+            to: new Twilio.Types.PhoneNumber("+92 312 1150399")
+        );
+
+        Console.WriteLine(message.Sid);
+    }
+}
+
